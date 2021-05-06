85aa5374385aeab1559d1d6ff08e4ee8fb544805
RyanTavcar <ryan.tavcar@gmail.com>
2021-05-06 12:53:33 +1000

    created card

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-//const Weather = require('weather.js')
+//const {Weather} = require('./weather');
+
+function Weather(currentWeather) {
+    this.currentTemp = currentWeather;
+    //this.date = date;
+}
+
+Weather.prototype.displayWeather = () => {
+    const weatherCard = document.getElementById('weather-card');
+    weatherCard.innerText = this.currentTemp;
+}
+
+
 
 const form = document.getElementById('form');
-
-
-const form = document.getElementById('form');
-const APIkey    = 'f18b6ae1c57f039f48f95ade89757557';
-
-let weatherObject ={}
+const APIkey = 'f18b6ae1c57f039f48f95ade89757557';
 
 const weatherObject = {}
 
@@ -14,7 +21,7 @@ const weatherObject = {}
 const getData = async () => {
     try {
         const cityName = inputValue();
-        const unit = 'metric'
+        const unit = 'metric' //unit
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&appid=${APIkey}`);
         const data = await response.json();
         return data
@@ -33,26 +40,40 @@ const apiError = (error) => {
 }
 
 const displayData = (data) => {
-    console.log(data)
+    console.log('first' + data)
     return data
 }
 
 const formHandler = (event) => {
     event.preventDefault();
     getData().then(displayData).catch(apiError);
-    asssembleData(getData);
+    asssembleData(getData).then(createWeatherObject);
+    //createWeatherObject();
 }
 
 const asssembleData = async (getData) => {
     const {main} = await getData();
-    weatherObject.currentTemp = main.temp;
-    console.log(weatherObject)
-    //const weather = new Weather()
+    weatherObject.currentTemp = await main.temp;
+    weatherObject.maxTemp = main.temp_max;
+    weatherObject.minTemp = main.temp_min;
+    console.log('second ' + weatherObject)
+
+    return 
 }
 
+const createWeatherObject =  async () => {
+    const currentTemp =  await weatherObject.currentTemp
+    console.log('third ' + currentTemp)
+    const weather =  await new Weather(currentTemp)
+    weather.displayWeather(currentTemp);
+
+    return
 }
 
-const createCard = () => {
-    console.log(weatherObject.currentTemp)
-    //const weather = new Weather()
-}
+
+// const createCard = () => {
+
+//     weather.displayWeather()
+// }
+
+form.addEventListener('submit', formHandler )
